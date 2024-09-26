dde3ae15b23091a4fe39b03c97b7bb0ac28acdf9
doe <doejung.jin@gmail.com>
2024-09-26 07:30:36 +0700

    update

---
+++ lib/states/authen.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingmall/utility/my_constant.dart';
+import 'package:shoppingmall/widgets/show_image.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
+
+class Authen extends StatefulWidget {
+  const Authen({super.key});
+
+  @override
+  State<Authen> createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
+  @override
+  Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(
+              FocusNode()), // focusscopr เพื่อให้จิ้มตรงไหนคียบอร์ดก็หายไปเอง แต่ต้องใช้ widget gesturedetector ด้วย
+          behavior: HitTestBehavior.opaque,
+          child: ListView(
+            children: [
+              buildImage(size),
+              buildAppName(),
+              buildUser(size),
+              buildPassword(size),
+              buildLogin(size),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildLogin(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          width: size * 0.6,
+          child: ElevatedButton(
+            style: MyConstant().myButtonStyle(),
+            onPressed: () {},
+            child: Text('Log In'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              label: Text('User'),
+              prefixIcon: Icon(
+                Icons.account_circle,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                },
+                icon: statusRedEye
+                    ? Icon(
+                        Icons.remove_red_eye,
+                        color: MyConstant.dark,
+                      )
+                    : Icon(
+                        Icons.remove_red_eye_outlined,
+                        color: MyConstant.dark,
+                      ),
+              ),
+              label: Text('Password'),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appNmae,
+          textStyle: MyConstant().h1Style(),
+        ),
+      ],
+    );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: ShowImage(path: MyConstant.image1),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/utility/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get theme {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      colorScheme: ColorScheme.light(
+        primary: Colors.green, // สีพื้นหลังหลักของแอป
+        onPrimary: Colors.white, // สีข้อความบนพื้นหลังหลัก
+        secondary: Colors.amber, // สีรอง
+        onSecondary: Colors.black, // สีข้อความบนสีรอง
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              Colors.blue.shade800, // สีพื้นหลังของปุ่ม ElevatedButton
+          foregroundColor: Colors.white, // สีข้อความในปุ่ม
+          minimumSize: Size(150, 50), // ขนาดขั้นต่ำของปุ่ม (กว้าง x สูง)
+          textStyle: TextStyle(
+            fontSize: 18, // ขนาดตัวอักษรของข้อความในปุ่ม
+            fontWeight: FontWeight.bold,
+          ), // น้ำหนักตัวอักษร
+        ),
+      ),
+    );
+  }
+}
